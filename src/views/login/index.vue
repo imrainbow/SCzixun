deb5f60f501a0db743b6767516d01cf1158cdc38
rainbow <rainbowcc.wang@gmail.com>
2022-06-21 16:32:13 +0800

    倒计时完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,33 +1,47 @@
 <template>
   <div class="login-container">
     <!-- 导航开始 -->
-     <van-nav-bar class="page-nav-bar" title="登陆" />
-     <!-- 导航结束 -->
-     <!-- 表单开始 -->
-     <van-form @submit="onSubmit">
-      <van-field
-        v-model="user.mobile"
-        name="mobile"
-        placeholder="请输入手机号"
-          >
-          <i slot="left-icon" class="iconfont">&#xe60c;</i>
+    <van-nav-bar class="page-nav-bar" title="登陆" />
+    <!-- 导航结束 -->
+    <!-- 表单开始 -->
+    <van-form
+      ref="loginForm"
+      @submit="onSubmit">
+      <van-field v-model="user.mobile" name="mobile" placeholder="请输入手机号" :rules="userFormRules.mobile" type="number" maxlength="11">
+        <i slot="left-icon" class="iconfont">&#xe60c;</i>
       </van-field>
       <van-field
         v-model="user.code"
         name="code"
         placeholder="请输入密码"
-        :rules="[{ required: true, message: '请填写密码' }]"
-       >
+        :rules="userFormRules.code"
+      >
         <i slot="left-icon" class="iconfont">&#xe622;</i>
         <template #button>
-          <van-button class="send-sms-btn" round size="small" type="default">获取验证码</van-button>
+          <van-count-down
+          v-if="isCountDownShow"
+          :time="1000*10"
+          format="ss s"
+          @finish="isCountDownShow = false"
+          />
+          <van-button
+            v-else
+            native-type="button"
+            class="send-sms-btn"
+            round size="small"
+            type="default"
+            @click="onSendSms"
+            >发送验证码</van-button
+          >
         </template>
       </van-field>
       <div class="login-btn-wrap">
-      <van-button class="login-btn" block type="info" native-type="submit">登陆</van-button>
+        <van-button class="login-btn" block type="info" native-type="submit"
+          >登陆</van-button
+        >
       </div>
-     </van-form>
-     <!-- 表单结束 -->
+    </van-form>
+    <!-- 表单结束 -->
   </div>
 </template>
 
@@ -40,7 +54,14 @@ export default {
       user: {
         mobile: '',
         code: ''
-      }
+      },
+      userFormRules: {
+        mobile: [{ required: true, message: '手机号不能为空' },
+          { pattern: /^1[3|5|7|8]\d{9}$/, message: '手机号格式错误' }],
+        code: [{ required: true, message: '请输入密码' },
+          { pattern: /^\d{6}$/, message: '密码格式错误' }]
+      },
+      isCountDownShow: false
     }
   },
   methods: {
@@ -48,22 +69,40 @@ export default {
       // 获取表单数据
       const user = this.user
       // 表单验证
+      // 在组件中必需通过this.$toast来调用toast组件
+      this.$toast.loading({
+        message: '加载中...',
+        forbidClick: true, // 禁用背景点击
+        duration: 0 // 持续时间，默认是2000，如果为0持续展示
+      })
       // 提交表单请求登陆
       try {
         const res = await login(user)
-        console.log('登陆成功', res)
+        console.log(res)
+        this.$toast.success('登陆成功')
       } catch (err) {
         if (err.response.status === 400) {
-          console.log('手机号或验证码错误')
+          this.$toast.fail('手机号或验证码错误')
         } else {
-          console.log('登陆失败', err)
+          this.$toast.fail('登陆失败请稍后再试')
         }
       }
 
       // 根据请求响应结果处理后续操作
+    },
+    async onSendSms () {
+      // 1.校验手机号
+      try {
+        await this.$refs.loginForm.validate('mobile')
+        console.log('验证通过')
+      } catch (err) {
+        return console.log('验证失败', err)
+      }
+      // 2.验证通过，显示倒计时
+      this.isCountDownShow = true
+      // 3.请求发送验证码
     }
   }
-
 }
 </script>
 
@@ -88,5 +127,4 @@ export default {
     }
   }
 }
-
 </style>
