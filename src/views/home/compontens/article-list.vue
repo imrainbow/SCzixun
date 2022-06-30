0effb4accd329307a155ddd5cfcb96930a68c24b
rainbow <rainbowcc.wang@gmail.com>
2022-06-30 10:57:18 +0800

    下拉刷新再次完成

--- src/views/home/compontens/article-list.vue
+++ src/views/home/compontens/article-list.vue
@@ -1,21 +1,26 @@
 <!--  -->
 <template>
-
   <div class="ariticle-list">
-    <van-list
-      v-model="loading"
-      :finished="finished"
-      finished-text="没有更多了"
-      :error.sync="error"
-      error-text="请求失败，点击重新加载"
-      @load="onLoad"
+    <van-pull-refresh
+      v-model="isLoading"
+      :success-text="refreshSuccessText"
+      @refresh="onRefresh"
     >
-      <van-cell
-        v-for="(article, index) in list"
-        :key="index"
-        :title="article.title"
-      />
-    </van-list>
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        :error.sync="error"
+        error-text="请求失败，点击重新加载"
+        @load="onLoad"
+      >
+        <van-cell
+          v-for="(article, index) in list"
+          :key="index"
+          :title="article.title"
+        />
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
@@ -79,6 +84,28 @@ export default {
         this.error = true
         this.loading = false
         console.log('请求失败', err)
+      }
+    },
+    async onRefresh() {
+      try {
+        // 请求获取数据
+        const { data } = await getArticles({
+          channel_id: this.channel.id,
+          timestamp: Date.now(),
+          with_top: 1
+        })
+        // 将请求的数据添加到顶部
+        const { results } = data.data
+        this.list.unshift(...results)
+        // 关闭下拉刷新状态
+        this.isLoading = false
+        // 提示刷新成功
+        this.refreshSuccessText = '刷新成功'
+        console.log('刷新成功')
+      } catch (err) {
+        console.log('刷新失败', err)
+        this.refreshSuccessText = '刷新失败'
+        this.isLoading = false
       }
     }
 
